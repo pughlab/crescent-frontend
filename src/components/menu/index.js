d5d0cc8d0005da9d70d782b93ef4b1be2ddbf132
Suluxan Mohanraj <suluxan.m@gmail.com>
2019-05-12 02:14:43 -0400

    added cards for viewing and downloading different runs

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,6 +1,6 @@
-import React, {useState, createRef, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 
-import {Menu, Card, Header, Segment, Button, Grid, Modal, Label, Divider, Icon} from 'semantic-ui-react'
+import {Menu, Card, Popup, Segment, Button, Grid, Modal, Label, Divider, Icon} from 'semantic-ui-react'
 
 import * as R from 'ramda'
 
@@ -12,31 +12,90 @@ const CrescentIcon = () => (
 )
 
 const RunsModal = ({
-
+  session,
+  currentRunId, setCurrentRunId
 }) => {
   const [openModal, setOpenModal] = useState(false)
+  const [runs, setRuns] = useState([])
+  useEffect(() => {
+    session
+      .call('crescent.runs', [], {})
+      .then(res => setRuns(res))
+  })
   return (
-    <Modal size='large'
+    <Modal size='fullscreen'
       open={openModal}
       trigger={
         <Menu.Item content='Runs' onClick={() => setOpenModal(true)}/>
       }
     > 
       <Modal.Header content="Runs" />
-      <Modal.Content>
+      <Modal.Content scrolling>
         <Card.Group itemsPerRow={3}>
         {
-          R.compose(
-            R.map(
-              n => (
-                <Card key={n}
-                  header={n}
-                  description='Description'
-                />
-              )
+          R.map(
+            ({runId, params}) => (
+              <Card key={runId} link>
+                <Card.Content>
+                  <Card.Header content={runId} />
+                  <Card.Description>
+                    {
+                      R.compose(
+                        ({
+                          singleCell,
+                          resolution,
+                          genes,
+                          opacity,
+                          principalDimensions,
+                          returnThreshold
+                        }) => (
+                          <Label.Group>
+                            <Label content='Single Cell Input Type' detail={singleCell} />
+                            <Label content='TSNE Resolution' detail={resolution} />
+                            <Label content='Opacity' detail={opacity} />
+                            <Label content='PCA Dimensions' detail={principalDimensions} />
+                            <Label content='Return Threshold' detail={returnThreshold} />
+                          </Label.Group>
+                        ),
+                        JSON.parse
+                      )(params)
+                    }
+                  </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                  <Label.Group>
+                  {
+                    R.compose(
+                      R.map(
+                        gene => <Label key={gene} content={gene} />
+                      ),
+                      R.prop('genes'),
+                      JSON.parse
+                    )(params)
+                  }
+                  </Label.Group>
+                </Card.Content>
+                <Card.Content extra>
+                  <Button.Group fluid widths={2} color='violet' size='large'>
+                    <Button icon='download' content='Download' labelPosition='left'
+                      as='a'
+                      href={`http://localhost:4001/download?runId=${runId}`}
+                      download
+                    />
+                    <Button.Or />
+                    <Button icon='eye' content='View' labelPosition='right'
+                      onClick={() => {
+                          setCurrentRunId(runId)
+                          setOpenModal(false)
+                        }
+                      }
+                    />
+                  </Button.Group>
+                </Card.Content>
+              </Card>
             ),
-            R.times(R.identity)
-          )(10)
+            runs
+          )
         }
         </Card.Group>
       </Modal.Content>
@@ -48,9 +107,9 @@ const RunsModal = ({
 }
 
 const MenuComponent = ({
-
+  session,
+  currentRunId, setCurrentRunId
 }) => {
-
   return (
     <Segment attached='bottom' style={{height: '10%'}} as={Menu} size='huge'>
       <Menu.Item header>
@@ -59,7 +118,10 @@ const MenuComponent = ({
       </Menu.Item>
 
       <Menu.Menu position='right'>
-        <RunsModal />
+        <RunsModal
+          session={session}
+          currentRunId={currentRunId} setCurrentRunId={setCurrentRunId}
+        />
       </Menu.Menu>
     </Segment>
   )
