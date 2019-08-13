fac4d93158acad55a54d6aa43e7b26599d950839
mdpham <martin.pham@sickkids.ca>
2019-08-13 10:01:00 -0400

    landing page local state temp

--- src/App.js
+++ src/App.js
@@ -6,20 +6,24 @@ import {Segment} from 'semantic-ui-react'
 import MenuComponent from './components/menu'
 import VisualizationComponent from './components/home'
 
+import LandingComponent from './components/landing'
+
 const App = ({
   session
 }) => {
   const [currentRunId, setCurrentRunId] = useState(null)
+  const [loggedIn, setLoggedIn] = useState(true)
   return (
     <Segment style={{height: '100%'}}>
-      <VisualizationComponent
-        session={session}
-        currentRunId={currentRunId} setCurrentRunId={setCurrentRunId}
-      />
-      <MenuComponent
-        session={session}
-        currentRunId={currentRunId} setCurrentRunId={setCurrentRunId}
-      />
+      {
+        loggedIn ?
+        <>
+          <VisualizationComponent session={session} currentRunId={currentRunId} setCurrentRunId={setCurrentRunId} />
+          <MenuComponent session={session} currentRunId={currentRunId} setCurrentRunId={setCurrentRunId} />
+        </>
+        :
+        <LandingComponent />
+      }
     </Segment>
   )
 }
---
+++ src/components/landing/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LandingPageComponent = () => {
+
+  return (
+    <>
+    Test
+    </>
+  )
+}
+
+export default LandingPageComponent
