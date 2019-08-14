a56bf12525c0d9c186f389e81f0901b0f2a40d8f
mdpham <martin.pham@sickkids.ca>
2019-08-14 11:14:07 -0400

    apollo provider react component

--- src/App.js
+++ src/App.js
@@ -8,18 +8,29 @@ import VisualizationComponent from './components/home'
 
 import LandingComponent from './components/landing'
 
+import { useQuery } from '@apollo/react-hooks'
+import { gql } from 'apollo-boost'
+
 const App = ({
   session
 }) => {
   const [currentRunId, setCurrentRunId] = useState(null)
   const [loggedIn, setLoggedIn] = useState(false)
+  const {loading, data, error} = useQuery(gql`
+    {
+      users {
+        userID
+      }
+    }
+  `)
+  console.log(loading, data, error)
   return (
-    !loggedIn ?
-    <LandingComponent /> :
-    <Segment style={{height: '100%'}}>
-      <VisualizationComponent session={session} currentRunId={currentRunId} setCurrentRunId={setCurrentRunId} />
-      <MenuComponent session={session} currentRunId={currentRunId} setCurrentRunId={setCurrentRunId} />
-    </Segment>
+      !loggedIn ?
+      <LandingComponent /> :
+      <Segment style={{height: '100%'}}>
+        <VisualizationComponent session={session} currentRunId={currentRunId} setCurrentRunId={setCurrentRunId} />
+        <MenuComponent session={session} currentRunId={currentRunId} setCurrentRunId={setCurrentRunId} />
+      </Segment>
   )
 }
 
