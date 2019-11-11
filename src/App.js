84ae03a8bd42c00c368e31f329a06864c411046b
mdpham <martin.pham@sickkids.ca>
2019-11-11 14:40:38 -0500

    remove landing modal

--- src/App.js
+++ src/App.js
@@ -19,19 +19,19 @@ const App = withRedux(
   }) => {
     const [introduction, setIntroduction] = useState(true)
     return (
-      introduction ?
-        <Modal open={introduction} dimmer='inverted'>
-          <Modal.Content>
-            <Image src={Logo} size='medium' centered/>
-          </Modal.Content>
-          <Modal.Content>
-            <Button animated='fade' fluid size='huge' color='grey' onClick={() => setIntroduction(false)}>
-              <Button.Content visible content={'CReSCENT: CanceR Single Cell ExpressioN Toolkit'} />
-              <Button.Content hidden content='Enter' />
-            </Button>
-          </Modal.Content>
-        </Modal>
-        :
+      // introduction ?
+        // <Modal open={introduction} dimmer='inverted'>
+        //   <Modal.Content>
+        //     <Image src={Logo} size='medium' centered/>
+        //   </Modal.Content>
+        //   <Modal.Content>
+        //     <Button animated='fade' fluid size='huge' color='grey' onClick={() => setIntroduction(false)}>
+        //       <Button.Content visible content={'CReSCENT: CanceR Single Cell ExpressioN Toolkit'} />
+        //       <Button.Content hidden content='Enter' />
+        //     </Button>
+        //   </Modal.Content>
+        // </Modal>
+        // :
         <Segment style={{height: '100%', padding: 0}}>
           <MenuComponent />
           <MainComponent {...{session}} />
