75b84247215c5400e3e97d57c3a8e4206664c1b4
mdpham <martin.pham@sickkids.ca>
2019-12-04 17:05:09 -0500

    disable button on initial submit click

--- src/components/main/vis/parameters/SubmitRunButton.js
+++ src/components/main/vis/parameters/SubmitRunButton.js
@@ -7,8 +7,11 @@ import * as RA from 'ramda-adjunct'
 
 import withRedux from '../../../../redux/hoc'
 
-import {useMutation} from '@apollo/react-hooks'
+import {useMutation, useQuery} from '@apollo/react-hooks'
 import { gql } from 'apollo-boost'
+
+import {queryIsNotNil} from '../../../../utils'
+
 
 
 const SubmitRunButton = withRedux(
@@ -28,7 +31,7 @@ const SubmitRunButton = withRedux(
     },
     actions: {}
   }) => {
-    const [submitRun, {loading, data, error}] = useMutation(gql`
+    const [submitRun, {loading: loadingSubmitRun, data: dataSubmitRun, error}] = useMutation(gql`
       mutation SubmitRun($runID: ID, $params: String) {
         submitRun(runID: $runID, params: $params) {
           runID
@@ -37,9 +40,32 @@ const SubmitRunButton = withRedux(
     `, {
       variables: {runID, params: JSON.stringify(parameters)}
     })
+    const {loading: loadingRun, data: dataRun, error: errorRun} = useQuery(gql`
+      query Run($runID: ID) {
+        run(runID: $runID) {
+          runID
+          status
+        }
+      }
+    `, {
+      variables: {runID}, 
+      fetchPolicy: 'network-only'
+    })
+    console.log(dataRun)
+    const runPreviouslySubmitted = R.ifElse(
+      queryIsNotNil('run'),
+      R.compose(R.not, R.propEq('status', 'pending'), R.prop('run')),
+      R.F
+    )(dataRun)
+    const runIsNotPending = R.not(R.equals('pending', runStatus))
+    const runSubmitted = R.or(
+      loadingSubmitRun,
+      queryIsNotNil('submitRun', dataSubmitRun)
+    )
+    console.log([loadingRun, runPreviouslySubmitted, runIsNotPending, runSubmitted])
     return (
       <Button fluid content={R.equals('pending', runStatus) ? 'SUBMIT RUN' : 'ALREADY SUBMITTED'} color='blue'
-        disabled={R.not(R.equals('pending', runStatus))}
+        disabled={R.any(RA.isTrue, [runPreviouslySubmitted, runIsNotPending, runSubmitted])}
         onClick={() => submitRun()}
       />
     )
