a954786db368d2ddb4f76fa4249ef0ee371faf2d
mdpham <martin.pham@sickkids.ca>
2019-12-05 04:46:13 -0500

    move project card meta info

--- src/components/main/projects/ProjectCard.js
+++ src/components/main/projects/ProjectCard.js
@@ -37,18 +37,16 @@ const ProjectCard = withRedux(({
     <Card link onClick={() => setProject(project)} color='grey'>
       <Card.Content>
         <Label attached='top' color='grey'>
-          <Icon name='folder open' size='large' style={{margin: 0}} />
+          <Icon name='folder open' size='large' style={{marginLeft: 0}} />
         </Label>
         <Card.Header>
           <Header size='small'>
             <Marquee text={name} />
-            <Header.Subheader>
-              {`Created by ${creatorName} on ${moment(createdOn).format('D MMMM YYYY')}`}
-            </Header.Subheader>
           </Header>
         </Card.Header>
       </Card.Content>
       <Card.Content extra content={description} />
+      <Card.Content extra content={`Created by ${creatorName} on ${moment(createdOn).format('D MMMM YYYY')}`} />
       <Card.Content>
         <Button.Group widths={3}>
         {
