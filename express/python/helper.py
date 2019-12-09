5a6733bdfc2d2e468f970737f889085ce45a1b53
helrick <hillary.elrick@sickkids.ca>
2019-12-09 00:51:49 +0000

    added 20k threshold for faster rending option

--- express/python/helper.py
+++ express/python/helper.py
@@ -1,9 +1,23 @@
 #!/bin/python
 
 import sys
+import os
 import json
 from operator import itemgetter
 
+def get_cellcount(runID):
+	""" given a runID, return the number of cells in the data """
+	path = "/usr/src/app/results/{runID}/SEURAT/groups.tsv".format(runID=runID) 
+	if not os.path.isfile(path):
+		# try command-line path
+		path = "../../results/{runID}/SEURAT/groups.tsv".format(runID=runID)
+		if not os.path.isfile(path):
+			return_error("group label file not found ("+path+")")	
+	
+	with open(path) as group_definitions:
+		for count, line in enumerate(group_definitions):
+			pass
+		return count # don't add one so header not counted
 
 def return_error(msg):
 	""" format the error message and perform a system flush before exiting """
