26dbfb2da05a706b72627beaa5d9dade7b185c73
mdpham <martin.pham@sickkids.ca>
2020-02-29 16:58:41 -0500

    move labels into popup

--- react/src/components/main/projects/ProjectCard.js
+++ react/src/components/main/projects/ProjectCard.js
@@ -54,9 +54,30 @@ const ProjectCard = withRedux(({
               </Button>
             }
             content={
-                <Segment>
-                {description}
-                </Segment>
+                <Segment.Group>
+                  <Segment>
+                    {description}
+                  </Segment>
+                  <Segment>
+                    <Label.Group>
+                      <Label content={<Icon style={{margin: 0}} name='user' />} detail={creatorName} />
+                      <Label content={<Icon style={{margin: 0}} name='calendar alternate outline' />} detail={moment(createdOn).format('DD MMM YYYY')} />
+                      {/* <Label content={<Icon style={{margin: 0}} name='certificate' />} detail={'1000000'} /> */}
+                      {/* <Label content={<Icon style={{margin: 0}} name='save' />} detail={filesize(datasetSize)} /> */}
+                      {/* <Label content={<Icon style={{margin: 0}} name='hashtag' />} detail='cancer' />
+                      <Label content={<Icon style={{margin: 0}} name='hashtag' />} detail='mouse' /> */}
+
+                      {
+                        RA.isNotEmpty(mergedProjects) &&
+                        <Label content={<Icon style={{margin: 0}} name='folder open' />} detail={R.length(mergedProjects)} />
+                      }
+                      {
+                        RA.isNotEmpty(uploadedDatasets) &&
+                        <Label content={<Icon style={{margin: 0}} name='upload' />} detail={R.length(uploadedDatasets)} />
+                      }
+                    </Label.Group>
+                  </Segment>
+                </Segment.Group>
             }
           />
         <Card.Content>
@@ -68,70 +89,59 @@ const ProjectCard = withRedux(({
             </Header>
           </Card.Header>
         </Card.Content>
-        <Card.Content>
-          <Label.Group>
-            <Label content={<Icon style={{margin: 0}} name='user' />} detail={creatorName} />
-            <Label content={<Icon style={{margin: 0}} name='calendar alternate outline' />} detail={moment(createdOn).format('DD MMM YYYY')} />
-            {/* <Label content={<Icon style={{margin: 0}} name='certificate' />} detail={'1000000'} /> */}
-            {/* <Label content={<Icon style={{margin: 0}} name='save' />} detail={filesize(datasetSize)} /> */}
-            {/* <Label content={<Icon style={{margin: 0}} name='hashtag' />} detail='cancer' />
-            <Label content={<Icon style={{margin: 0}} name='hashtag' />} detail='mouse' /> */}
-
-            {
-              RA.isNotEmpty(mergedProjects) &&
-              <Label content={<Icon style={{margin: 0}} name='folder open' />} detail={R.length(mergedProjects)} />
-            }
-            {
-              RA.isNotEmpty(uploadedDatasets) &&
-              <Label content={<Icon style={{margin: 0}} name='upload' />} detail={R.length(uploadedDatasets)} />
-            }
-          </Label.Group>
-        </Card.Content>
-        <Card.Content>
-          <Label.Group size='medium'>
-          {
-            R.compose(
-              R.flatten,
-              R.props(['pending', 'submitted', 'completed', 'failed']),
-              R.mapObjIndexed((runs, status) => {
-                const statusColor = R.prop(status, {
-                  pending: 'orange',
-                  submitted: 'yellow',
-                  completed: 'green',
-                  failed: 'red'
-                })
-                if (R.isEmpty(runs)) {
-                  return null
+        {
+          R.ifElse(
+            R.isEmpty,
+            R.always(null),
+            runs => (
+              <Card.Content>
+                <Label.Group size='medium'>
+                {
+                  R.compose(
+                    R.flatten,
+                    R.props(['pending', 'submitted', 'completed', 'failed']),
+                    R.mapObjIndexed((runs, status) => {
+                      const statusColor = R.prop(status, {
+                        pending: 'orange',
+                        submitted: 'yellow',
+                        completed: 'green',
+                        failed: 'red'
+                      })
+                      if (R.isEmpty(runs)) {
+                        return null
+                      }
+                      return (
+                        <Label key={status} color={statusColor}>
+                          <Icon
+                            name={R.prop(status, {
+                              pending: 'circle outline',
+                              submitted: 'circle notch',
+                              completed: 'circle outline check',
+                              failed: 'times circle outline'
+                            })}
+                            loading={R.equals('submitted', status)}
+                          />
+                          {R.length(runs)}
+                          {' '}
+                          {R.toUpper(status)}
+                        </Label>
+                      )
+                    }),
+                    R.reduce(
+                      (runsByStatus, run) => R.over(
+                        R.lensProp(R.prop('status', run)),
+                        R.append(run),
+                        runsByStatus
+                      ),
+                      {pending: [], submitted: [], completed: [], failed: []}
+                    )
+                  )(runs)
                 }
-                return (
-                  <Label key={status} color={statusColor}>
-                    <Icon
-                      name={R.prop(status, {
-                        pending: 'circle outline',
-                        submitted: 'circle notch',
-                        completed: 'circle outline check',
-                        failed: 'times circle outline'
-                      })}
-                      loading={R.equals('submitted', status)}
-                    />
-                    {R.length(runs)}
-                    {' '}
-                    {R.toUpper(status)}
-                  </Label>
-                )
-              }),
-              R.reduce(
-                (runsByStatus, run) => R.over(
-                  R.lensProp(R.prop('status', run)),
-                  R.append(run),
-                  runsByStatus
-                ),
-                {pending: [], submitted: [], completed: [], failed: []}
-              )
-            )(runs)
-          }
-          </Label.Group>
-        </Card.Content>
+                </Label.Group>
+              </Card.Content>
+            )
+          )(runs)
+        }
       </Card>
     </Transition>
   )
