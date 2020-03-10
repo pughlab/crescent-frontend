72a1deb33b29eabc5a0a91b8723cf02f8f516aac
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2020-03-10 13:17:57 -0400

    Add Bioconductor as a dependency of Seurat

--- crescent/install-base.R
+++ crescent/install-base.R
@@ -1,5 +1,5 @@
 #!/usr/bin/env Rscript
 
-install.packages(c('Seurat','fmsb','optparse','staplr','devtools'), dependencies=TRUE)
+install.packages(c('Bioconductor','Seurat','fmsb','optparse','staplr','devtools'), dependencies=TRUE)
 devtools::install_github(repo = 'hhoeflin/hdf5r')
 devtools::install_github(repo = 'mojaveazure/loomR', ref = 'develop')
