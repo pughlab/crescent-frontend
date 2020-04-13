8f0b4121c13de874693a79ae7a78bff098908403
samarthvp <samarth28@hotmail.com>
2020-04-13 15:41:31 -0400

    Basic job file creation in WesCall.py to be moved

--- express/python/WesCall.py
+++ express/python/WesCall.py
@@ -15,12 +15,37 @@ if inFile.mode == "r":
     job = inFile.read()
 
 # Job creation needs to occur here instead of in submit.js for when we move from express to flask
+# job = {
+#     "R_script": {
+#         "class": "File",
+#         "path": "Script/Runs_Seurat_v3.R"
+#     },
+#     "R_dir": {
+#         "class": "Directory",
+#         "path": "Script"
+#     },
+#     "sc_input_type": "MTX", # should come from input
+#     "resolution": 1,
+#     "project_id": "frontend_example_mac_10x_cwl",
+#     "summary_plots": "n",
+#     "pca_dimensions": 10, # should come from input
+#     "percent_mito": "0,0.2", # should come from input
+#     "number_genes": "50,8000", # should come from input
+#     "minioInputPath": "minio/project-5e4eeb54fee1bc004ceda65f/inputs/", # should come from input
+#     "destinationPath": "minio/project-5e4eeb54fee1bc004ceda65f/runs/5e947ad79846b0010c6fad78", # should come from input
+#     "access_key": "crescent-access", # should come from env vars
+#     "secret_key": "crescent-secret", # should come from env vars
+#     "minio_domain": "host.docker.internal",
+#     "minio_port": "9000"
+# }
 
+job = json.dumps(job)
 
 # make request to wes
 clientObject = util.WESClient(
     {'auth': '', 'proto': 'http', 'host': "wes-server:8081"})
 
 # use seurat-workflow.cwl
+# All workflow related files must be passed as attachments here, excluding files in minio
 req = clientObject.run(
     pathToCWL + "seurat-workflow.cwl", job, [pathToScript + "Runs_Seurat_v3.R", pathToCWL + "extract.cwl", pathToCWL + "seurat-v3.cwl", pathToCWL + "upload.cwl", pathToCWL + "clean.cwl"])
