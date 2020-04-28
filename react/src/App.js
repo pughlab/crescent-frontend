1d7536631d000dc668c6e3d8cf728a6c2908ee13
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2020-04-28 18:02:38 -0400

    Remove unnecessary Redux dependency from App

--- react/src/App.js
+++ react/src/App.js
@@ -1,22 +1,17 @@
-import './App.css'
 import 'semantic-ui-css/semantic.min.css'
-import React from 'react'
+import './App.css'
 
+import React from 'react'
 import { Segment } from 'semantic-ui-react'
 
 import MenuComponent from './components/menu'
 import MainComponent from './components/main'
 
-import withRedux from './redux/hoc'
-
-const App = withRedux(
-  () => {
-    return (
-      <Segment style={{padding: 0}}>
-        <MenuComponent />
-        <MainComponent />
-      </Segment>
-    )
-  }
-)
-export default App
+export default function App() {
+  return (
+    <Segment style={{padding: 0}}>
+      <MenuComponent />
+      <MainComponent />
+    </Segment>
+  )
+}
