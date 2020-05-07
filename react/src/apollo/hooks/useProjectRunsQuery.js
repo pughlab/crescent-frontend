332076a41301d29298b94c8fb5bd20e52b7f8569
mdpham <martin.pham@sickkids.ca>
2020-05-06 23:30:00 -0400

    add runsStatusLegend to runsPage with runsFilter hook

---
+++ react/src/apollo/hooks/useProjectRunsQuery.js
@@ -0,0 +1,42 @@
+import {useState} from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import { gql } from 'apollo-boost'
+
+
+export default function useProjectRunsQuery(projectID) {
+  const [projectRuns, setProjectRuns] = useState([])
+  const {loading, data, error} = useQuery(gql`
+    query ProjectRuns($projectID: ID) {
+      runs(projectID: $projectID) {
+        runID
+        createdOn
+        createdBy {
+          userID
+          name
+        }
+        name
+        params
+        status
+
+        submittedOn
+        completedOn
+
+        datasets {
+          datasetID
+          name
+          size
+          hasMetadata
+        }
+      }
+    }
+  `, {
+    fetchPolicy: 'network-only',
+    variables: {projectID},
+    onCompleted: ({runs}) => {
+      console.log('runs', runs)
+      setProjectRuns(runs)
+    }
+  })
+
+  return projectRuns
+}
