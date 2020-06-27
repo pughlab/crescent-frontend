90842812a787f23730c7600a1af8c30568ef481d
Suluxan Mohanraj <suluxan.m@gmail.com>
2020-06-27 17:43:14 -0400

    use QCAvailableQuery and QCMetricsQuery with hooks

---
+++ react/src/apollo/hooks/useQCAvailableQuery.js
@@ -0,0 +1,61 @@
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import {createUploadLink} from 'apollo-upload-client'
+
+
+import {useState} from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import { gql } from 'apollo-boost'
+
+require('dotenv').config()
+console.log(process.env.REACT_APP_GRAPHENE_URL_DEV)
+
+const link = createUploadLink({uri: process.env.NODE_ENV === 'development'
+? process.env.REACT_APP_GRAPHENE_URL_DEV
+// TODO :prod url
+: process.env.REACT_APP_GRAPHENE_URL_DEV})
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link
+})
+
+
+// export default function useGraphene() {
+//   const {loading, data, error} = useQuery(gql`
+//     query {
+//       test {
+//         testField
+//       }
+//     }
+//   `, {
+//     client,
+//   })
+//   console.log(data)
+//   return 
+// }
+
+export default function useQCAvailable(runID) {
+  const [availableQc, setAvailableQC] = useState(null)
+  const {loading, data, error} = useQuery(gql`
+    query QCAvailable($runID: ID) {
+      availableQc(runID: $runID) {
+        data {
+          key
+          text
+          value
+        }
+      }
+    }
+    `, {
+    client,
+    fetchPolicy: 'cache-and-network',
+    variables: {runID},
+    onCompleted: ({availableQc}) => {
+      setAvailableQC(availableQc)
+    }
+  })
+
+  return availableQc
+}
+
