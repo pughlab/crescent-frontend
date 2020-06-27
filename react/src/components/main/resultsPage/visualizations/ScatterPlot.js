10dd8e61ae185a9e834927ab56a267b82f528a2c
Suluxan Mohanraj <suluxan.m@gmail.com>
2020-06-27 17:45:05 -0400

    scatter template

---
+++ react/src/components/main/resultsPage/visualizations/ScatterPlot.js
@@ -0,0 +1,19 @@
+import React, {useState, useEffect, useCallback } from 'react'
+import Plot from 'react-plotly.js'
+import withRedux from '../../../../redux/hoc'
+import { Loader, Segment, Header, Icon } from 'semantic-ui-react'
+
+import {ClimbingBoxLoader} from 'react-spinners'
+
+import * as R from 'ramda'
+import * as RA from 'ramda-adjunct'
+
+const ScatterPlot = ({
+
+}) => {
+  return (
+    <Loader></Loader>
+  )
+}
+
+export default ScatterPlot 
