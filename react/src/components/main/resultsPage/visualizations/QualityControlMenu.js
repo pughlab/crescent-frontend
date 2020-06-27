ec7aa2db1d6db7e06ff11de493665105b3bece1f
Suluxan Mohanraj <suluxan.m@gmail.com>
2020-06-27 17:44:45 -0400

    QC menu with availableQC and QCMetrics

---
+++ react/src/components/main/resultsPage/visualizations/QualityControlMenu.js
@@ -0,0 +1,163 @@
+import React, {useState, useEffect } from 'react'
+import Plot from 'react-plotly.js'
+import withRedux from '../../../../redux/hoc'
+import { Button, Form, Dropdown, Segment, Popup, Label, Icon, Header, Grid, Divider } from 'semantic-ui-react'
+
+import * as R from 'ramda'
+import * as RA from 'ramda-adjunct'
+
+import {useDispatch} from 'react-redux'
+import {useCrescentContext, useResultsPage} from '../../../../redux/hooks'
+import {useQCAvailableQuery, useQCMetricsQuery} from '../../../../apollo/hooks'
+import {setSelectedQC} from '../../../../redux/actions/resultsPage'
+
+const QualityControlMenu = ({
+}) => {    
+  const {runID} = useCrescentContext()
+
+  const dispatch = useDispatch()
+  const {selectedQC} = useResultsPage()
+  // const isActiveResult = R.equals(activeResult)
+
+  const availableQc = useQCAvailableQuery(runID)
+  const qcMetrics = useQCMetricsQuery(runID)
+
+  if (R.any(R.isNil, [availableQc, qcMetrics])) {
+    return null
+  }
+
+  const {cellcounts: cellcounts} = qcMetrics
+  const {qcSteps: qcSteps} = qcMetrics
+
+
+
+  // const [AvailableQCPlots, setAvailableQCPlots] = useState([])
+  // var [qcMetrics, setMetrics] = useState([])
+
+
+  // useEffect(() => {
+  //   fetchAvailableQC(runID).then((data) => {
+  //     R.ifElse(
+  //       R.has('error'),
+  //       R.always(console.log(data['error'])),
+  //       initializeQC
+  //     )(data)
+  //   });
+  // }, [])
+
+  // useEffect(() => {
+  //   fetchMetrics(runID).then((data) => {
+  //     R.ifElse(
+  //       R.has('error'),
+  //       R.always(console.log(data['error'])),
+  //       setMetrics
+  //     )(data)
+  //   })
+
+  //   return setMetrics([])
+
+  // }, [runID])
+
+  // const initializeQC = (availableQc) => {
+  //   // set the dropdown values
+  //   // setAvailableQCPlots(availableQc)
+  //   R.isNil(selectedQC) ? dispatch(setSelectedQC({availableQc})) : dispatch(setSelectedQC({selectedQC}))
+  // }
+  
+  // var qcSteps = R.ifElse(
+  //   R.or(R.isNil,R.isEmpty), 
+  //   R.always([]), 
+  //   R.always(qcMetrics[1])
+  // )(qcMetrics)
+
+  const availableQcArray = R.compose(
+    R.head,
+    R.values
+  )(availableQc)
+
+  // console.log(R.values(availableQcArray))
+
+  return (
+    <>
+    <Divider horizontal content='QC Plot Type' />
+    <Dropdown
+      selection
+      fluid
+      labeled
+      options={availableQcArray}
+      // options = {dropdown_plots}
+
+      onChange={(e, {value}) => dispatch(setSelectedQC({value}))}
+
+      defaultValue={RA.isNotNilOrEmpty(selectedQC) ? selectedQC : ''}
+    />
+
+    <Divider horizontal content='QC Metrics' />
+    <Segment basic textAlign='center' style={{padding: '0rem'}}>
+    <Label basic color={'blue'} size={'large'}>
+      Cells Before QC:
+      <Label.Detail>
+        {
+          R.ifElse(
+            R.or(R.isNil, R.isEmpty),
+            R.always(""),
+            ({Before}) => {return Before}
+          )(cellcounts)
+
+        }
+      </Label.Detail>
+    </Label>
+    </Segment>
+
+    {
+      R.ifElse(
+        R.isNil,
+        R.always(<div></div>),
+        R.addIndex(R.map)(
+          ({filtertype, num_removed, min, max}, index) => (
+            <Segment compact key={index}>
+              <Label attached='top' content={filtertype}/>
+              <Button
+              basic
+              size={'tiny'}
+              style={{margin: '0.25rem'}}
+              content={`Filter Min: `+min}
+              />
+              <Button
+              basic
+              size={'tiny'}
+              style={{margin: '0.25rem'}}
+              content={`Filter Max: `+max}
+              />
+              <Button
+              basic
+              size={'tiny'}
+              style={{margin: '0.25rem'}}
+              content={`Cells Removed: `+num_removed}
+              color={R.ifElse(R.equals("0"),R.always(undefined),R.always('red'))(num_removed)}
+              />
+            </Segment>
+        ))
+      )(qcSteps)
+
+    }
+    <Segment basic textAlign='center' style={{padding: '0rem'}}>
+    <Label basic color={'orange'} size={'large'}>
+      Cells After QC:
+      <Label.Detail>
+        {
+          R.ifElse(
+            R.or(R.isNil, R.isEmpty),
+            R.always(""),
+            ({After}) => {return After}
+          )(cellcounts)
+
+        }
+      </Label.Detail>
+    </Label>
+    </Segment>
+    </>
+  )
+}
+
+export default QualityControlMenu
