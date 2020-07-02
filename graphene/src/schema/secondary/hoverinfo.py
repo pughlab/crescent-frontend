4cfa20178592ae3a41922a963bc49eda72d9c3ea
Prabnur Bal <p3bal@uwaterloo.ca>
2020-07-02 18:43:46 -0400

    Changed some comments to bettetr explain design

--- graphene/src/schema/secondary/hoverinfo.py
+++ graphene/src/schema/secondary/hoverinfo.py
@@ -1,32 +1,15 @@
 from graphene import Enum, List, Union
 
 # https://plotly.com/javascript/reference/#violin-hoverinfo
-# Any combination of HoverInfoPiece joined by + or a HoverInfoWhole
-"""
-class HoverInfoPiece(Enum):
-    x = "x"
-    y = "y"
-    z = "z"
-    text = "text"
-    name = "name"
-
-class HoverInfoWhole(Enum):
-    all = "all"
-    none = "none"
-    skip = "skip"
-
-class HoverInfo(Union):
-    class Meta:
-        types = (List(HoverInfoPiece), HoverInfoWhole)
-"""
 class HoverInfo(Enum):
+    # Any combination of these joined by +
     x = "x"
     y = "y"
     z = "z"
     text = "text"
     name = "name"
-
+    
+    # Or one of these
     all = "all"
     none = "none"
     skip = "skip"
-# """
