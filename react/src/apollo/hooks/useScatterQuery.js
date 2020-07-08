bd5a8f8e390aa907992d15bb46eda5c22fefe6e0
Suluxan Mohanraj <suluxan.m@gmail.com>
2020-07-08 02:40:44 -0400

    scatter component and hook WIP

--- react/src/apollo/hooks/useScatterQuery.js
+++ react/src/apollo/hooks/useScatterQuery.js
@@ -1,23 +1,75 @@
-// import { ApolloClient } from 'apollo-client'
-// import { InMemoryCache } from 'apollo-cache-inmemory'
-// import {createUploadLink} from 'apollo-upload-client'
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import {createUploadLink} from 'apollo-upload-client'
 
 
-// import {useState} from 'react'
-// import { useQuery } from '@apollo/react-hooks'
-// import { gql } from 'apollo-boost'
+import {useState} from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import { gql } from 'apollo-boost'
+import * as R from 'ramda'
 
-// require('dotenv').config()
-// console.log(process.env.REACT_APP_GRAPHENE_URL_DEV)
 
-// const link = createUploadLink({uri: process.env.NODE_ENV === 'development'
-// ? process.env.REACT_APP_GRAPHENE_URL_DEV
-// // TODO :prod url
-// : process.env.REACT_APP_GRAPHENE_URL_DEV})
+require('dotenv').config()
+console.log(process.env.REACT_APP_GRAPHENE_URL_DEV)
 
-// const client = new ApolloClient({
-//   cache: new InMemoryCache(),
-//   link
-// })
+const link = createUploadLink({uri: process.env.NODE_ENV === 'development'
+? process.env.REACT_APP_GRAPHENE_URL_DEV
+// TODO :prod url
+: process.env.REACT_APP_GRAPHENE_URL_DEV})
 
- export default function useScatterQuery(runID) {}
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link
+})
+
+
+// export default function useGraphene() {
+//   const {loading, data, error} = useQuery(gql`
+//     query {
+//       test {
+//         testField
+//       }
+//     }
+//   `, {
+//     client,
+//   })
+//   console.log(data)
+//   return 
+// }
+
+export default function useScatter(vis, group, runID) {
+  const [scatter, setScatter] = useState(null)
+  const {loading, data, error} = useQuery(gql`
+    query Scatter($vis: String, $group: String, $runID: ID) {
+      scatter(vis: $vis, group: $group, runID: $runID) {
+        data {
+            name
+            mode
+            text
+            x
+            y
+            marker {
+                color
+            }
+        }
+      }
+    }
+    `, {
+    client,
+    fetchPolicy: 'cache-and-network',
+    variables: {vis, group, runID},
+    onCompleted: ({scatter}) => {
+      R.compose(
+        setScatter,
+        R.map(R.evolve({
+          data: {
+            mode: R.join('+')
+          }
+        }))
+      )(scatter)
+    }
+  })
+
+  return scatter
+}
+
--- react/src/components/main/resultsPage/visualizations/ScatterPlot.js
+++ react/src/components/main/resultsPage/visualizations/ScatterPlot.js
@@ -1,37 +1,145 @@
 import React, {useState, useEffect, useCallback } from 'react'
 import Plot from 'react-plotly.js'
 import withRedux from '../../../../redux/hoc'
-import { Loader, Segment, Header, Icon } from 'semantic-ui-react'
-
+import { Loader, Container, Header, Segment, Dimmer} from 'semantic-ui-react'
 import {ClimbingBoxLoader} from 'react-spinners'
-import {useDispatch} from 'react-redux'
-import {useCrescentContext, useResultsPage} from '../../../../redux/hooks'
-import {useScatterQuery} from '../../../../apollo/hooks'
-import {setSelectedQC} from '../../../../redux/actions/resultsPage'
 
 import * as R from 'ramda'
-import * as RA from 'ramda-adjunct'
+
+import {useDispatch} from 'react-redux'
+import {useCrescentContext, useResultsPage} from '../../../../redux/hooks'
+import {useResultsAvailableQuery, useScatterQuery} from '../../../../apollo/hooks'
+import {setSelectedQC} from '../../../../redux/actions/resultsPage'
 
 const ScatterPlot = ({
-}) => { 
-
+}) => {
   const {runID} = useCrescentContext()
+
   const dispatch = useDispatch()
-  const {selectedQC} = useResultsPage()
-  
+  const {activeResult, selectedFeature, selectedGroup} = useResultsPage()
 
- // use local state for data since too big for redux store
-//  const [scatterData, setScatterData] = useState( [] );
-//  const [isLoading, setIsLoading] = useState( true );
+  const plots = useResultsAvailableQuery(runID)
+  const scatter = useScatterQuery(activeResult, selectedGroup, runID)
 
-//need to make a scatterData = useScatterData
-//loading stuff unclear atm
+  console.log(scatter)
+  console.log(plots)
 
-const scatterData = useScatterQuery(runID)
-//const qcMetrics = useQCMetricsQuery(runID)
+
+
+  if (R.any(R.isNil, [plots, scatter])) {
+    return null
+  }
+
+  // const scatterData = R.compose(
+  //   R.evolve({
+  //     data: {
+  //       mode: R.join('+')
+  //     }
+  //   })
+  // )(scatter)
+  // console.log(scatterData)
+
+  // scatterData = scatter
+
+  // const scatterData = R.compose(
+  //   R.head,
+  //   R.values,
+  // )(scatter)
+  // console.log(scatterData)
+
+  const scatterData = scatter
+
+
+
+  // use local state for data since too big for redux store
+  // const [scatterData, setScatterData] = useState( [] );
+  // const [isLoading, setIsLoading] = useState( true );
+
+  const isLoading = false
+  // // func to knit opacity from server into traces
+  // const addOpacity = (traces) => {
+  //   return new Promise ((res, rej) => {
+  //     fetchOpacity()
+  //     .then(data => {
+  //       const merged = R.ifElse(
+  //         R.has('error'),
+  //         () => {console.error(data['error']); res(traces)}, // show error on plot here
+  //         R.addIndex(R.map)((val, index) => R.mergeLeft(val, traces[index]))
+  //       )(data)
+  //       res(merged)
+  //     })
+  //   })
+  // }
+
+  // // determine proper name of active plot
+  const currentScatterPlotType = R.ifElse(
+    R.isNil,
+    R.always(''),
+    R.path([activeResult, 'label'])
+  )(plots)
+
+  // // add traces and opacity
+  // useEffect(() => {
+  //   setIsLoading(true)
+  //   fetchScatter().then((data) => {
+  //     if(! R.or(R.isNil,R.isEmpty)(selectedFeature)){
+  //       addOpacity(data)
+  //       .then((merged) => {
+  //         setScatterData(merged);
+  //         setIsLoading(false);
+  //       })
+  //     }
+  //     else{
+  //       setScatterData(data);
+  //       setIsLoading(false);
+  //     }
+  //   })
+  // }, [activeResult,selectedGroup])
+
+  // // add or clear opacity from plot
+  // useEffect(() => {
+  //   if(isLoading){return} // the other hook is already dealing with this 
+  //   setIsLoading(true)
+  //   let prev = scatterData
+  //   if(! R.or(R.isNil,R.isEmpty)(selectedFeature)){
+  //      addOpacity(prev)
+  //     .then((merged) => {
+  //       setScatterData(merged);
+  //       setIsLoading(false);
+  //     })
+  //   }
+  //   else{
+  //     fetchScatter().then((data) => {
+  //       setScatterData(data);
+  //       setIsLoading(false); 
+  //     })
+  //   }
+  // }, [selectedFeature])
+
 
   return (
-    <Loader></Loader>
+    <>
+    <Dimmer.Dimmable dimmed={isLoading} style={{height:'100%'}}>
+    <Dimmer active={isLoading} inverted>
+      <ClimbingBoxLoader color='#6435c9' />
+    </Dimmer>
+    <Header textAlign='center' content={currentScatterPlotType} />
+      <Plot
+        config={{showTips: false}}
+        data={scatterData}
+        useResizeHandler
+        style={{width: '100%', height:'100%'}}
+        layout={{
+          autosize: true,
+          hovermode: 'closest',
+          xaxis: {showgrid: false, ticks: '', showticklabels: false},
+          yaxis: {showgrid: false, ticks: '', showticklabels: false, scaleanchor: "x"},
+          margin: {l:20, r:20, b:20, t:20},
+          legend: {"orientation": "v"}
+        }}
+      />
+    </Dimmer.Dimmable>
+    </>
   )
 }
 
