a7471dca93a307d17c7bcfd516374af1fd507feb
mdpham <martin.pham@sickkids.ca>
2020-08-30 19:45:59 -0400

    run dataset options query hook

--- react/src/apollo/hooks/useRunDatasetsQuery.js
+++ react/src/apollo/hooks/useRunDatasetsQuery.js
@@ -1,6 +1,8 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import { useQuery } from '@apollo/react-hooks'
 import { gql } from 'apollo-boost'
+import * as RA from 'ramda-adjunct'
+import * as R from 'ramda'
 
 export default function useRunDatasetsQuery(runID) {
   const [run, setRun] = useState(null)
@@ -27,4 +29,27 @@ export default function useRunDatasetsQuery(runID) {
   })
 
   return run
+}
+
+// Hook for providing dropdown selection options
+// runID => [...{key, text, value}]
+export function useRunDatasetsDropdownQuery(runID, {onNonEmptyOptions}) {
+  const [options, setOptions] = useState(null)
+  const run = useRunDatasetsQuery(runID)
+  useEffect(() => {
+    if (RA.isNotNil(run)) {
+      R.compose(
+        setOptions,
+        R.map(({datasetID, name}) => ({key: datasetID, value: datasetID, text: name})),
+        R.prop('datasets')
+      )(run)
+    }
+  }, [run])
+
+  useEffect(() => {
+    if (R.not(RA.isNilOrEmpty(options))) {
+      onNonEmptyOptions(options)
+    }
+  }, options)
+  return options
 }
