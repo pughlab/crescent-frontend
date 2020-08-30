7b0113f0a3648a9fb7e498f369fda17aca3bf14c
mdpham <martin.pham@sickkids.ca>
2020-08-30 19:53:37 -0400

    use run datasets dropdown query, clean up qcmetrics hooks

--- react/src/components/main/resultsPage/visualizations/QualityControlMenu.js
+++ react/src/components/main/resultsPage/visualizations/QualityControlMenu.js
@@ -5,187 +5,85 @@ import { Button, Form, Dropdown, Segment, Popup, Label, Icon, Header, Grid, Divi
 
 import * as R from 'ramda'
 import * as RA from 'ramda-adjunct'
+import * as R_ from 'ramda-extension'
 
 import {useDispatch} from 'react-redux'
 import {useCrescentContext, useResultsPage} from '../../../../redux/hooks'
 import {useRunDatasetsQuery, useQCAvailableQuery, useQCMetricsQuery} from '../../../../apollo/hooks'
 import {setSelectedQC, setSelectedQCDataset} from '../../../../redux/actions/resultsPage'
 
+import {useRunDatasetsDropdownQuery} from '../../../../apollo/hooks/useRunDatasetsQuery'
+
 const QualityControlMenu = ({
   runID
 }) => {    
-  // const {runID} = useCrescentContext()
-
   const dispatch = useDispatch()
   const {selectedQCDataset, selectedQC} = useResultsPage()
-  // const isActiveResult = R.equals(activeResult)
-
-  // const run = useRunDatasetsQuery(runID)
-
-  const run = useRunDatasetsQuery(runID)
-
-  // if (R.isNil(run)) {
-  //   return null
-  // }
+  const datasetsOptions = useRunDatasetsDropdownQuery(runID, {
+    onNonEmptyOptions: options => {
+      const [{value}] = options
+      dispatch(setSelectedQCDataset({value}))
+    }
+  })
   
-  // const availableQc = useQCAvailableQuery({runID, selectedQCDataset})
-  // const qcMetrics = useQCMetricsQuery({runID, selectedQCDataset})
+  const availableQc = useQCAvailableQuery({runID, datasetID: selectedQCDataset})
+  const qcMetrics = useQCMetricsQuery({runID, datasetID: selectedQCDataset})
 
-  // if (R.any(R.isNil, [run, availableQc, qcMetrics])) {
-  //   return null
-  // }
-  if (R.any(R.isNil, [run])) {
+  if (R.any(R.isNil, [datasetsOptions, availableQc])) {
     return null
   }
 
-  const {datasets} = run
-  // const {cellcounts: cellcounts} = qcMetrics
-  // const {qcSteps: qcSteps} = qcMetrics
-
-  const datasetIDs = R.pluck('datasetID')(datasets)
-
-
-  console.log('selected dataset: ',selectedQCDataset)
-  console.log('datasetIDs: ', datasetIDs)
-  // const [AvailableQCPlots, setAvailableQCPlots] = useState([])
-  // var [qcMetrics, setMetrics] = useState([])
-
-
-  // useEffect(() => {
-  //   fetchAvailableQC(runID).then((data) => {
-  //     R.ifElse(
-  //       R.has('error'),
-  //       R.always(console.log(data['error'])),
-  //       initializeQC
-  //     )(data)
-  //   });
-  // }, [])
-
-  // useEffect(() => {
-  //   fetchMetrics(runID).then((data) => {
-  //     R.ifElse(
-  //       R.has('error'),
-  //       R.always(console.log(data['error'])),
-  //       setMetrics
-  //     )(data)
-  //   })
-
-  //   return setMetrics([])
-
-  // }, [runID])
-
-  // const initializeQC = (availableQc) => {
-  //   // set the dropdown values
-  //   // setAvailableQCPlots(availableQc)
-  //   R.isNil(selectedQC) ? dispatch(setSelectedQC({availableQc})) : dispatch(setSelectedQC({selectedQC}))
-  // }
-  
-  // var qcSteps = R.ifElse(
-  //   R.or(R.isNil,R.isEmpty), 
-  //   R.always([]), 
-  //   R.always(qcMetrics[1])
-  // )(qcMetrics)
-
-  // const availableQcArray = R.compose(
-  //   R.head,
-  //   R.values
-  // )(availableQc)
-
-  // console.log(R.values(availableQcArray))
-
-  const formatList = R.addIndex(R.map)((val, index) => ({key: index, text: val, value: val}))
 
   return (
     <>
-    {/* <Divider horizontal content='QC Plot Type' />
-    <Dropdown
-      selection
-      fluid
-      labeled
+    <Divider horizontal content='QC Plot Type' />
+    <Dropdown selection fluid labeled
       options={availableQc}
-      // options = {dropdown_plots}
-
+      value={selectedQC}
       onChange={(e, {value}) => dispatch(setSelectedQC({value}))}
-
-      defaultValue={RA.isNotNilOrEmpty(selectedQC) ? selectedQC : ''}
-    /> */}
+    />
     <Divider horizontal content='Datasets' />
-    <Dropdown
-      selection
-      fluid
-      labeled
-      options={formatList(datasetIDs)}
-      // options = {dropdown_plots}
+    <Dropdown selection fluid labeled search
+      loading={R.isNil(datasetsOptions)}
+      options={datasetsOptions || []}
       value={selectedQCDataset}
-      defaultValue={RA.isNotNil(selectedQCDataset) ? selectedQCDataset : dispatch(setSelectedQCDataset({value: datasetIDs[0]}))}
-
       onChange={(e, {value}) => dispatch(setSelectedQCDataset({value}))}
-
-      // defaultValue={RA.isNotNilOrEmpty(selectedQC) ? selectedQC : ''}
     />
 
-    {/* <Divider horizontal content='QC Metrics' />
-    <Segment basic textAlign='center' style={{padding: '0rem'}}>
-    <Label basic color={'blue'} size={'medium'}>
-      Cells Before QC:
-      <Label.Detail>
-        {
-          R.ifElse(
-            R.or(R.isNil, R.isEmpty),
-            R.always(""),
-            ({Before}) => {return Before}
-          )(cellcounts)
-
-        }
-      </Label.Detail>
-    </Label>
-    <Label basic color={'orange'} size={'medium'} >
-      Cells After QC:
-      <Label.Detail>
-        {
-          R.ifElse(
-            R.or(R.isNil, R.isEmpty),
-            R.always(""),
-            ({After}) => {return After}
-          )(cellcounts)
-
-        }
-      </Label.Detail>
-    </Label>
-    </Segment>
     {
       R.ifElse(
         R.isNil,
-        R.always(<div></div>),
-        R.addIndex(R.map)(
-          ({filtertype, numRemoved, min, max}, index) => (
-            <Segment compact key={index} style={{padding: '0.77rem'}} >
-              <Label attached='top' content={filtertype}/>
-              <Label
-              basic
-              size={'small'}
-              style={{margin: '0.25rem'}}
-              content={`Filter Min: `+min}
-              color='grey'
-              />
-              <Label
-              basic
-              size={'small'}
-              style={{margin: '0.25rem'}}
-              content={`Filter Max: `+max}
-              color='grey'
-              />
-              <Label
-              basic
-              size={'small'}
-              style={{margin: '0.25rem'}}
-              content={`Cells Removed: `+numRemoved}
-              color={R.ifElse(R.equals(0),R.always('grey'),R.always('red'))(numRemoved)}
-              />
-            </Segment>
-        ))
-      )(qcSteps)
-    } */}
+        R_.alwaysNull,
+        ({cellcounts: {Before, After}, qcSteps}) => (
+          <>
+          <Divider horizontal content='QC Metrics' />
+          <Segment basic textAlign='center' style={{padding: '0rem'}}>
+          <Label basic color={'blue'} size={'medium'}
+            content='Cells Before QC:'
+            detail={Before}
+          />
+          <Label basic color={'orange'} size={'medium'}
+            content='Cells After QC:'
+            detail={After}
+          />
+          </Segment>
+          {
+            R.addIndex(R.map)(
+              ({filtertype, numRemoved, min, max}, index) => (
+                <Segment compact key={index} style={{padding: '0.77rem'}} >
+                  <Label attached='top' content={filtertype}/>
+                  <Label basic size={'small'} style={{margin: '0.25rem'}} content={`Filter Min: ${min}`} color='grey' />
+                  <Label basic size={'small'} style={{margin: '0.25rem'}} content={`Filter Max: ${max}`} color='grey' />
+                  <Label basic size={'small'} style={{margin: '0.25rem'}} content={`Cells Removed: ${numRemoved}`} color={R.ifElse(R.equals(0),R.always('grey'),R.always('red'))(numRemoved)} />
+                </Segment>
+              ),
+              qcSteps
+            )
+          }
+          </>
+        )
+      )(qcMetrics)
+    }
     </>
   )
 }
