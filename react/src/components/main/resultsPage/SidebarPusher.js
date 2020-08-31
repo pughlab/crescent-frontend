59c8bcf8c29c1426d00b8a26f36c88e86819566f
mdpham <martin.pham@sickkids.ca>
2020-08-31 06:00:24 -0400

    top scroll

--- react/src/components/main/resultsPage/SidebarPusher.js
+++ react/src/components/main/resultsPage/SidebarPusher.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import * as R from 'ramda'
 
 import { Segment, Transition, Grid, Image, Message, Label,   Checkbox,
@@ -10,7 +10,10 @@ import { Segment, Transition, Grid, Image, Message, Label,   Checkbox,
   Container,
   Form,
   Dimmer,
-  Popup, } from 'semantic-ui-react'
+  Ref,
+  Sticky,
+  Popup,
+  Divider, } from 'semantic-ui-react'
 
 import SidebarComponent from './sidebar'
 import ParametersComponent from './parameters'
@@ -35,9 +38,25 @@ const ResultsPageSidebarPusher = ({
 
   // Move this into redux
   const [visible, setVisible] = React.useState(true)
+
+  const stickyRef = useRef()
+
+  const [showScroll, setShowScroll] = useState(false)
+
+  window.addEventListener('scroll', 
+    () => {
+      if (!showScroll && window.pageYOffset > 200){
+        setShowScroll(true)
+      } else if (showScroll && window.pageYOffset <= 200){
+        setShowScroll(false)
+      }
+    }
+  )
+
   return (
-    <Grid stretched>
-      <Grid.Column width={visible ? 10 : 15}>
+    <Ref innerRef={stickyRef}>
+    <Grid>
+      <Grid.Column width={visible ? 10 : 15} stretched>
       {
         R.cond([
           [R.equals('parameters'), R.always(<ParametersComponent />)],
@@ -45,29 +64,64 @@ const ResultsPageSidebarPusher = ({
         ])(activeSidebarTab)
       }
       </Grid.Column>
-      <Grid.Column width={1}>
+
+      
+      <Grid.Column width={1} stretched>
+      <Sticky context={stickyRef}>
         <Popup
           position='left center'
           inverted
           on='hover'
           trigger={
-            <Button
+            <Button fluid
+              animated='fade'
               color='black' basic
-              icon={visible ? 'right arrow' : 'left arrow'}
               onClick={() => setVisible(!visible)}
-            />
+            >
+              <Button.Content visible content={<Icon name={visible ? 'right arrow' : 'left arrow'} />} />
+              <Button.Content hidden content={<Icon name={visible ? 'eye slash' : 'eye'} />} />
+            </Button>
           }
           content={visible ? 'Hide menu' : 'Show menu'}
         />
+
+        <Divider horizontal />
+        {
+          showScroll &&
+          <Popup
+            position='bottom center'
+            inverted
+            on='hover'
+            trigger={
+              <Button fluid
+                animated='fade'
+                color='black' basic
+                onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})}
+              >
+                <Button.Content visible content={<Icon name={'up arrow'} />} />
+                <Button.Content hidden content={<Icon name={'window maximize outline'} />} />
+              </Button>
+            }
+            content={'Click to scroll to top'}
+          />
+        }
+
+      </Sticky>
+
+      
       </Grid.Column>
+      
 
       {
         visible && 
-        <Grid.Column width={5}>
+        <Grid.Column width={5} stretched>
+          <Fade right>
           <SidebarComponent />
+          </Fade>
         </Grid.Column>
       }
     </Grid>
+    </Ref>
   )
 }
 
