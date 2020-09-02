2f1125466190f9853837e652adbe30f8474bcd52
mdpham <martin.pham@sickkids.ca>
2020-09-02 14:56:36 -0400

    get dataset name in plot caption

--- react/src/components/main/resultsPage/visualizations/PlotCaption.js
+++ react/src/components/main/resultsPage/visualizations/PlotCaption.js
@@ -5,12 +5,16 @@ import {Segment, Icon, Header, Image, Grid, Label} from 'semantic-ui-react'
 import Shake from 'react-reveal/Shake'
 
 import {useDispatch} from 'react-redux'
+import {useCrescentContext} from '../../../../redux/hooks'
 import {useResultsPagePlotQuery} from '../../../../redux/hooks/useResultsPage'
+import {useRunDatasetsQuery} from '../../../../apollo/hooks'
 
 export default function CrescentPlotCaption ({
   plotQueryIndex
 }) {
   const plotQuery = useResultsPagePlotQuery(plotQueryIndex)
+  const {runID} = useCrescentContext()
+  const run = useRunDatasetsQuery(runID)
   const {
     activeResult,
     selectedQC,
@@ -21,14 +25,24 @@ export default function CrescentPlotCaption ({
 
   } = plotQuery
 
+  if (R.isNil(run)) {
+    return null
+  }
+
+  const dataset = R.ifElse(R.isNil, R.identity,
+    R.ifElse(R.equals('All'), R.identity,
+      datasetID => R.compose(R.prop('name'), R.find(R.propEq('datasetID', datasetID)))(run.datasets)
+    )
+  )(selectedQCDataset || selectedDiffExpression)
+
   const labels = R.compose(
     R.reject(R.where({detail: R.isNil}))
   )([
     {content: 'Plot', detail: R.toUpper(activeResult)},
-    {content: 'Dataset', detail: selectedQCDataset || selectedDiffExpression},
+    {content: 'Quality control', detail: R.equals('qc', activeResult) ? selectedQC : null},
+    {content: 'Dataset', detail: dataset},
     {content: 'Colour by', detail: selectedGroup},
     {content: 'Feature', detail: selectedFeature},
-    {content: 'Quality control', detail: R.equals('qc', activeResult) ? selectedQC : null}
   ])
 
   return (
