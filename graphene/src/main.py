49f38395357e9c83a1745b177d11cfe065d00109
Suluxan Mohanraj <suluxan.m@gmail.com>
2020-09-04 14:05:50 -0400

    all origins in fastAPI middleware

--- graphene/src/main.py
+++ graphene/src/main.py
@@ -8,7 +8,8 @@ from query import Query
 app = FastAPI()
 app.add_middleware(
   CORSMiddleware,
-  allow_origins=["http://localhost:3000"],
+  # allow_origins=["http://localhost:3000"],
+  allow_origins=["*"],
   allow_credentials=True,
   allow_methods=["*"],
   allow_headers=["*"]
