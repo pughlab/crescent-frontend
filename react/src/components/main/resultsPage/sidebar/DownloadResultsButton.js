fb85d7a3b65a2f5f8239178f35c544ee196360d1
Suluxan Mohanraj <suluxan.m@gmail.com>
2020-09-04 12:14:34 -0400

    download results button reimplemented

--- react/src/components/main/resultsPage/sidebar/DownloadResultsButton.js
+++ react/src/components/main/resultsPage/sidebar/DownloadResultsButton.js
@@ -2,12 +2,42 @@ import React from 'react'
 
 import {Button} from 'semantic-ui-react'
 
+import * as R from 'ramda'
+import * as RA from 'ramda-adjunct'
+
+import {useCrescentContext, useResultsPage} from '../../../../redux/hooks'
+import {useRunDetailsQuery} from '../../../../apollo/hooks'
+
+
 const DownloadResultsButton = ({
 
 }) => {
+  const {runID} = useCrescentContext()
+  const run = useRunDetailsQuery(runID)
+  
+  if (R.any(R.isNil, [run])) {
+    return null
+  }
+
+  const {status: runStatus} = run
+
   return (
     <Button fluid color='violet'
-      content='DOWNLOAD'
+      content={R.prop(runStatus, {
+        pending: 'RESULTS UNAVAILABLE',
+        // missing 'submitted' because RefreshRunButton rendered if run status is so
+        completed: 'DOWNLOAD RESULTS',
+        failed: 'DOWNLOAD RUN LOGS'
+      })}
+      disabled={
+        // R.or(R.either(R.equals('pending'), R.equals('submitted'))(runStatus)),(RA.isFalse(downloadable))
+        R.either(R.equals('pending'), R.equals('submitted'))(runStatus)
+      }
+      download
+      target='_blank'
+      // Should only work with nginx reverse proxy
+      // see: https://github.com/suluxan/crescent-frontend/commit/8300e985804518ce31e1de9c3d3b340ee94de3f6
+      href={`/express/download/${runID}`}
     />
   )
 }
