67173b87dc08793d4b06d8612042b9950c3e0b65
Suluxan Mohanraj <suluxan.m@gmail.com>
2020-09-04 05:31:24 -0400

    update opacity schema to include numeric opacity fields (wip)

--- graphene/src/schema/opacity.py
+++ graphene/src/schema/opacity.py
@@ -1,9 +1,18 @@
-from graphene import Field, Float, List, NonNull, ObjectType, String
+from graphene import Field, Float, List, NonNull, ObjectType, String, Boolean
 
 from schema.secondary.hex_colour_code import HexColour
 from schema.secondary.mode import Mode
 from schema.secondary.unitinterval import UnitInterval
+from schema.secondary.colorscale import ColorScaleMapping
 
+def isArrayNotNumerical(color_array):
+    for color in color_array:
+        try:
+            int(color)
+        except ValueError:
+            return True
+    return False
+    
 class OpacityMarker(ObjectType):
     opacity = List(UnitInterval)
     @staticmethod
@@ -14,6 +23,36 @@ class OpacityMarker(ObjectType):
     @staticmethod
     def resolve_color(parent, info):
         return parent["color"]
+    
+    colorscale = List(ColorScaleMapping)
+    @staticmethod
+    def resolve_colorscale(parent, info):
+        if (isArrayNotNumerical(parent["color"])):
+            return None
+        
+        # Plotly states that it needs a mapping for the numbers 0 and 1
+        # https://plotly.com/javascript/reference/#scatter-marker-colorscale
+        cs = parent["colorscale"]
+        zero_mapping_exists = False
+        unit_mapping_exists = False
+
+        for ui, col in cs:
+            if (ui == 0):
+                zero_mapping_exists = True
+            if (ui == 1):
+                unit_mapping_exists = True
+
+        if (zero_mapping_exists and unit_mapping_exists):
+            return cs
+        else:
+            return None
+    
+    showscale = Boolean()
+    @staticmethod
+    def resolve_showscale(parent, info):
+        if (isArrayNotNumerical(parent["color"])):
+            return None
+        return parent["showscale"]
 
 class OpacityData(ObjectType):
     marker = Field(OpacityMarker)
@@ -45,6 +84,11 @@ class OpacityData(ObjectType):
     @staticmethod
     def resolve_y(parent, info):
         return parent["y"]
+
+    hovertext = List(String)
+    @staticmethod
+    def resolve_hovertext(parent, info):
+        return parent["hovertext"]
 
     type = String()
     @staticmethod
