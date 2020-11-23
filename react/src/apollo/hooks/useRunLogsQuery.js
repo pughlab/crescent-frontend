32329b13daf64861742759e4f5489a5c333919eb
SamarthVP <samarth28@hotmail.com>
2020-11-23 10:12:08 -0500

    Created hook for run logs

---
+++ react/src/apollo/hooks/useRunLogsQuery.js
@@ -0,0 +1,28 @@
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { createUploadLink } from 'apollo-upload-client'
+
+import { useState, useEffect } from 'react'
+import { useQuery, useMutation } from '@apollo/react-hooks'
+import { gql } from 'apollo-boost'
+
+export default function useRunLogsQuery(runID) {
+  const [logs, setLogs] = useState(null)
+
+  const { loading, data, error } = useQuery(gql`
+    query RunLogs($runID: ID) {
+      run(runID: $runID) {
+        logs
+      }
+    }
+  `, {
+    variables: {
+      runID
+    },
+    onCompleted: ({ run: { logs } }) => {
+      setLogs(logs)
+    }
+  })
+
+  return {logs}
+}
