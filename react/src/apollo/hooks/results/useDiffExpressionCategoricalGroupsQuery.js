11dd13bfab650919ddfb0aa93b5048bac6ec575a
mdpham <martin.pham@sickkids.ca>
2021-02-18 16:07:17 -0500

    move diff exp hooks into results dir

--- react/src/apollo/hooks/results/useDiffExpressionCategoricalGroupsQuery.js
+++ react/src/apollo/hooks/results/useDiffExpressionCategoricalGroupsQuery.js
@@ -1,23 +1,25 @@
 import {useState, useEffect} from 'react'
-
 import * as RA from 'ramda-adjunct'
 
 import useCategoricalGroupsQuery from './useCategoricalGroupsQuery'
 
-import {setSelectedGroup} from '../../../redux/actions/resultsPage'
+import {setSelectedGroup, setSelectedDiffExpression} from '../../../redux/actions/resultsPage'
 
 import {useDispatch} from 'react-redux'
+import {useResultsPage} from '../../../redux/hooks'
+import { useResultsPagePlotQuery } from '../../../redux/hooks/useResultsPage'
 
 
 export default function useDiffExpressionCategoricalGroups(runID, selectedDiffExpression) {
   const dispatch = useDispatch()
-  // const diffExpression = useDiffExpressionQuery(runID)
   const categoricalGroups = useCategoricalGroupsQuery(runID, selectedDiffExpression)
 
+  const { activePlot } = useResultsPage()
+  const { selectedGroup } = useResultsPagePlotQuery(activePlot)
   // assume groups is never empty 
 
   useEffect(() => {
-    if (RA.isNotNil(categoricalGroups)) {
+    if (RA.isNotNil(categoricalGroups) && RA.isNilOrEmpty(selectedGroup)) {
       dispatch(setSelectedGroup({value: categoricalGroups[0]}))  
     }
   }, [categoricalGroups])
--- react/src/apollo/hooks/results/useDiffExpressionGroupsQuery.js
+++ react/src/apollo/hooks/results/useDiffExpressionGroupsQuery.js
@@ -1,21 +1,26 @@
 import {useState, useEffect} from 'react'
+
 import * as RA from 'ramda-adjunct'
 
 import useAvailableGroupsQuery from './useAvailableGroupsQuery'
 
-import {setSelectedGroup} from '../../../redux/actions/resultsPage'
+import {setSelectedGroup, setSelectedDiffExpression} from '../../../redux/actions/resultsPage'
 
 import {useDispatch} from 'react-redux'
+import { useResultsPage } from '../../../redux/hooks'
+import { useResultsPagePlotQuery } from '../../../redux/hooks/useResultsPage'
+
 
 export default function useDiffExpressionGroups(runID, selectedDiffExpression) {
   const dispatch = useDispatch()
-  // const diffExpression = useDiffExpressionQuery(runID)
   const groups = useAvailableGroupsQuery(runID, selectedDiffExpression)
 
+  const { activePlot } = useResultsPage()
+  const { selectedGroup } = useResultsPagePlotQuery(activePlot)
   // assume groups is never empty 
 
   useEffect(() => {
-    if (RA.isNotNil(groups)) {
+    if (RA.isNotNil(groups) && RA.isNilOrEmpty(selectedGroup)) {
       dispatch(setSelectedGroup({value: groups[0]}))  
     }
   }, [groups])
