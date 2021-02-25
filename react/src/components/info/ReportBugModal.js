e90a9d033004bc3ae69613bc4b93337928f6e180
suluxan <suluxan.m@gmail.com>
2021-02-25 05:22:56 -0500

    new report bug interface with google forms and github links

--- react/src/components/info/ReportBugModal.js
+++ react/src/components/info/ReportBugModal.js
@@ -5,14 +5,37 @@ import {Embed, Header, Segment, Button, Grid, Modal, Label, Divider, Icon, Image
 import * as R from 'ramda'
 import * as RA from 'ramda-adjunct'
 
+// const ReportBugModal = ({
+// }) => {
+//   return (
+//     <Popup inverted size='large'
+//       trigger={
+//         <Button color='grey' basic inverted icon size='large' 
+//         target='_blank'
+//         href={'https://github.com/pughlab/crescent-frontend/issues/new/choose'}
+//         >
+//           <Icon color='black' size='big' name='bug' />
+//         </Button>
+//       }
+//       content={
+//         'Report Bug'
+//       }
+//     />
+//   )
+// }
+
 const ReportBugModal = ({
 }) => {
+  const [open, setOpen] = useState(false)
+  const [activeMenu, setActiveMenu] = useState('forms')
+  const isActiveMenu = R.equals(activeMenu)
+
   return (
+    <>
     <Popup inverted size='large'
       trigger={
-        <Button color='grey' basic inverted icon size='large' 
-        target='_blank'
-        href={'https://github.com/pughlab/crescent-frontend/issues/new/choose'}
+        <Button color='grey' basic inverted icon size='large'
+          onClick={() => setOpen(!open)} 
         >
           <Icon color='black' size='big' name='bug' />
         </Button>
@@ -21,6 +44,69 @@ const ReportBugModal = ({
         'Report Bug'
       }
     />
+    <Modal
+      {...{open}}
+      closeIcon
+      onClose={() => setOpen(!open)}
+      // closeOnDimmerClick={false}
+
+    >
+      <Modal.Content>
+        <Divider horizontal>
+          Send us a bug report/feature request or email us at <a target="_blank" href='mailto:crescent@uhnresearch.ca' >crescent@uhnresearch.ca</a> 
+        </Divider>          
+        <Button.Group fluid widths={2}>
+          <Button content='Google Forms'
+            icon='google'
+            active={isActiveMenu('forms')} 
+            color={R.equals(activeMenu, 'forms') ? 'black' : undefined}
+            onClick={() => setActiveMenu('forms')}
+          />
+          <Button.Or />
+          <Button content='GitHub'
+            icon='github'
+            active={isActiveMenu('github')} 
+            color={R.equals(activeMenu, 'github') ? 'black' : undefined}
+            onClick={() => setActiveMenu('github')}
+          />
+        </Button.Group>
+        {
+          isActiveMenu('forms') ?
+          <Segment>
+            <Button fluid basic color='red' size='massive'
+              content='Bug Report'
+              target='_blank'
+              href={'https://forms.gle/Bd1jbyjYUno51qs18'}
+            >
+            </Button> 
+            <Divider/>
+            <Button fluid basic color='green' size='massive'
+              content='Feature Request'
+              target='_blank'
+              href={'https://forms.gle/WAheMM3UP9GwibPv5'}
+            >
+            </Button> 
+          </Segment>
+          :
+          <Segment>
+            <Button fluid basic color='red' size='massive' 
+              content='Bug Report'
+              target='_blank'
+              href={'https://github.com/pughlab/crescent-frontend/issues/new?assignees=&labels=&template=bug_report.md&title=%5BBUG%5D'}
+            >
+            </Button> 
+            <Divider/>
+            <Button fluid basic color='green' size='massive'
+              content='Feature Request'
+              target='_blank'
+              href={'https://github.com/pughlab/crescent-frontend/issues/new?assignees=&labels=&template=feature_request.md&title=%5BFEATURE%5D'}
+            >
+            </Button> 
+          </Segment>
+        }
+      </Modal.Content>
+    </Modal>
+    </>
   )
 }
 
