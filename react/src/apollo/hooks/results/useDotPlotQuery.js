dcc019071818daf456f305e14acde93f8f492459
Anna Guo <annaguo8010@gmail.com>
2021-05-27 15:22:42 -0400

    add a xstate machine for handling dot plot state changes

--- react/src/apollo/hooks/results/useDotPlotQuery.js
+++ react/src/apollo/hooks/results/useDotPlotQuery.js
@@ -5,7 +5,10 @@ import * as R from 'ramda'
 
 import {grapheneClient as client} from '../../clients'
 
+import { useDotPlotMachine } from '../../../components/main/resultsPage/visualizations/DotPlot'
+
 export default function useDotPlot(features, group, runID, scaleBy, expRange, assay, sidebarCollapsed) {
+  const [current, send] = useDotPlotMachine();
   const [dotPlot, setDotPlot] = useState(null)
   const { loading, data, error, refetch } = useQuery(gql`
     query DotPlot($features: [String], $group: String, $runID: ID, $scaleBy: String, $expRange: [Float], $assay: String, $sidebarCollapsed: Boolean) {
@@ -35,10 +38,7 @@ export default function useDotPlot(features, group, runID, scaleBy, expRange, as
     fetchPolicy: 'cache-and-network',
     variables: { features, group, runID, scaleBy, expRange, assay, sidebarCollapsed},
     onCompleted: ({ dotPlot }) => {
-      R.compose(
-        setDotPlot,
-        R.map(R.evolve({ mode: R.join('+') })),
-      )(dotPlot)
+      send({type: "SUCCESS", data: R.map(R.evolve({ mode: R.join('+') }), dotPlot)})
     },
     skip: R.any(R.isNil,[group, assay])
   })
@@ -49,6 +49,5 @@ export default function useDotPlot(features, group, runID, scaleBy, expRange, as
     }
   }, [error])
 
-  return {dotPlot, loading}
 }
 
