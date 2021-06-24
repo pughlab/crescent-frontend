162224a516c3210d45f079c7af65f0dc0a4015a7
Anna Guo <annaguo8010@gmail.com>
2021-06-24 10:03:21 -0400

    tweak dot plot to use state machine

--- react/src/apollo/hooks/results/useDotPlotQuery.js
+++ react/src/apollo/hooks/results/useDotPlotQuery.js
@@ -2,15 +2,20 @@ import { useState, useEffect } from 'react'
 import { useQuery } from '@apollo/react-hooks'
 import { gql } from 'apollo-boost'
 import * as R from 'ramda'
+import { useService } from '@xstate/react'
 
 import {grapheneClient as client} from '../../clients'
 
-import { useDotPlotMachine } from '../../../components/main/resultsPage/visualizations/DotPlot'
+import { useDispatch } from 'react-redux'
+import { useResultsPagePlotQuery } from '../../../redux/hooks/useResultsPage'
+import { sendSuccess } from '../../../redux/actions/resultsPage'
 
-export default function useDotPlot(features, group, runID, scaleBy, expRange, assay, sidebarCollapsed) {
-  const [current, send] = useDotPlotMachine();
-  const [dotPlot, setDotPlot] = useState(null)
-  const { loading, data, error, refetch } = useQuery(gql`
+export default function useDotPlot(features, group, runID, scaleBy, expRange, assay, sidebarCollapsed, plotQueryIndex) {
+  const dispatch = useDispatch()
+  const { service } = useResultsPagePlotQuery(plotQueryIndex)
+  const [current, send] = useService(service)
+  
+  const { data, error, refetch } = useQuery(gql`
     query DotPlot($features: [String], $group: String, $runID: ID, $scaleBy: String, $expRange: [Float], $assay: String, $sidebarCollapsed: Boolean) {
       dotPlot(features: $features, group: $group, runID: $runID, scaleBy:$scaleBy, expRange: $expRange, assay: $assay, sidebarCollapsed: $sidebarCollapsed) {
         type
@@ -38,7 +43,7 @@ export default function useDotPlot(features, group, runID, scaleBy, expRange, as
     fetchPolicy: 'cache-and-network',
     variables: { features, group, runID, scaleBy, expRange, assay, sidebarCollapsed},
     onCompleted: ({ dotPlot }) => {
-      send({type: "SUCCESS", data: R.map(R.evolve({ mode: R.join('+') }), dotPlot)})
+      dispatch(sendSuccess({ send, data: R.map(R.evolve({ mode: R.join('+') }), dotPlot) }))
     },
     skip: R.any(R.isNil,[group, assay])
   })
