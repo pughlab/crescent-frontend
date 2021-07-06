5dacee1274b378c88f21cb3ed65e3149890e5cb7
Grace <grace.xu@uwaterloo.ca>
2021-07-06 14:42:53 +0000

    add ability to change project ownership

--- react/src/components/menu/ProjectHeader.js
+++ react/src/components/menu/ProjectHeader.js
@@ -1,23 +1,29 @@
 import React, { useReducer, useEffect } from 'react'
 import * as R from 'ramda'
 import * as RA from 'ramda-adjunct'
-import { Button, Input, Form, Modal, Segment, Header, Label } from 'semantic-ui-react'
+import { Button, Input, Form, Modal, Dropdown, Segment, Header, Label } from 'semantic-ui-react'
 
 import { useCrescentContext } from '../../redux/hooks'
 import { useProjectDetailsQuery } from '../../apollo/hooks/project'
 import { useEditProjectDetailsMutation } from '../../apollo/hooks/project'
 
-
-//Example of custom hook
 function useProjectDetails(projectID) {
-  //const projectQuery = useProjectDetailsQuery(projectID) //query data from import { compose, graphql } from 'react-apollo'
-  const { project: projectQuery, editProjectDescription, editProjectName, loading, dataDesc, dataName } = useEditProjectDetailsMutation({ projectID })
+  const {
+    project: projectQuery,
+    editProjectDescription,
+    editProjectName,
+    changeProjectOwnership,
+    loading,
+    dataDesc,
+    dataName,
+    dataOwner
+  } = useEditProjectDetailsMutation({ projectID })
 
   // hold all state (project data, form data, modal open) in reducer
   const [state, dispatch] = useReducer((state, action) => {
     const { type, payload } = action
     if (type === 'resetForm') {
-      const form = { name: state.project.name, description: state.project.description }
+      const form = { name: state.project.name, description: state.project.description, ownerID: state.project.createdBy.userID }
       return { ...state, form }
     } else if (type === 'updateFormName') {
       const { newName } = payload
@@ -27,21 +33,34 @@ function useProjectDetails(projectID) {
       const { newDescription } = payload
       const form = { ...state.form, description: newDescription }
       return { ...state, form }
+    } else if (type === 'updateFormOwner') {
+      const { newOwnerID } = payload
+      const form = { ...state.form, ownerID: newOwnerID }
+      return { ...state, form }
     } else if (type === 'setProjectData') {
       const { project } = payload
-      const { name, description } = project
-      const form = { name, description }
+      const { name, description, sharedWith, createdBy } = project
+      const form = { name, description, ownerID: createdBy.userID }
       return { ...state, project, form }
     } else if (type === 'setOpen') {
       const { isOpen } = payload
       const open = isOpen
       return { ...state, open }
+    } else if (type === 'mutateName') {
+      editProjectName({ variables: { newName: state.form.name } })
+      return state
+    } else if (type === 'mutateDesc') {
+      editProjectDescription({ variables: { newDescription: state.form.description } })
+      return state
+    } else if (type === 'mutateOwner') {
+      changeProjectOwnership({ variables: { userID: state.form.ownerID } })
+      return state
     } else {
       return state
     }
   }, {
     project: null,
-    form: { name: '', description: '' },
+    form: { name: '', description: '', ownerID: '' },
     open: false
   })
 
@@ -52,39 +71,46 @@ function useProjectDetails(projectID) {
     }
   }, [projectQuery])
 
-  // when modal is open, set new name and description to match the old ones
+  // when modal is open, set new name, description, owner to match the old ones
   useEffect(() => {
     if (state.open) {
       dispatch({ type: 'resetForm' })
     }
-  }, [state.open])   
+  }, [state.open])
 
   // close Modal when mutation successful
-  useEffect(() => { if (R.or(RA.isNotNil(dataDesc), RA.isNotNil(dataName))) { dispatch({ type: 'setOpen', payload: { isOpen: false } }) } }, [dataDesc, dataName]) 
+  useEffect(() => {
+    if (R.any(RA.isTrue, [RA.isNotNil(dataDesc), RA.isNotNil(dataName), RA.isNotNil(dataOwner)])) {
+      dispatch({ type: 'setOpen', payload: { isOpen: false } })
+    }
+  }, [dataDesc, dataName, dataOwner])
 
-  return { state, dispatch, loading, editProjectDescription, editProjectName } 
+  return { state, dispatch, loading }
 }
 
 const ProjectHeader = ({
 
 }) => {
   const { projectID } = useCrescentContext()
-  const { state, dispatch, loading, editProjectDescription, editProjectName } = useProjectDetails(projectID)
+  const { state, dispatch, loading } = useProjectDetails(projectID)
 
   if (R.isNil(state.project)) {
     return null
   }
-  const oldName = state.project.name
-  const oldDesc = state.project.description
-  const sameName = R.equals(oldName, state.form.name)
-  const sameDesc = R.equals(oldDesc, state.form.description)
-  const disabled = R.or(loading, R.and(sameName, sameDesc)) // disable button when loading or unchanged description/name
 
-  // call appropriate mutate functions
+  const oldName = state.project.name
+  const sameName = R.equals(oldName, state.form.name)
+  const sameDesc = R.equals(state.project.description, state.form.description)
+  const sameOwner = R.equals(state.project.createdBy.userID, state.form.ownerID)
+  const disabled = R.or(loading, RA.allEqualTo(true, [sameName, sameDesc, sameOwner])) // disable button when loading or unchanged description/name/owner
+
+  // call dispatch with appropriate action(s) after Save is clicked
   const submitButtonHandler = () => {
-    if (!sameName) { editProjectName({ variables: { newName: state.form.name } }) }
-    if (!sameDesc) { editProjectDescription({ variables: { newDescription: state.form.description } }) }
+    if (!sameName) { dispatch({ type: 'mutateName' }) }
+    if (!sameDesc) { dispatch({ type: 'mutateDesc' }) }
+    if (!sameOwner) { dispatch({ type: 'mutateOwner' }) }
   }
+
   return (
     <Modal basic open={state.open} onOpen={() => dispatch({ type: 'setOpen', payload: { isOpen: true } })} onClose={() => dispatch({ type: 'setOpen', payload: { isOpen: false } })}
       trigger={
@@ -109,9 +135,21 @@ const ProjectHeader = ({
                 onChange={(e, { value }) => { dispatch({ type: 'updateFormDescription', payload: { newDescription: value } }) }}
               />
             </Form>
+            <Header as='h4'>Project Owner</Header>
+            <Dropdown placeholder='Select a User' fluid selection onChange={(e, { value }) => { dispatch({ type: 'updateFormOwner', payload: { newOwnerID: value } }) }}
+              options={
+                R.map(
+                  ({ name, userID }) => ({
+                    key: userID,
+                    value: userID,
+                    text: name
+                  }),
+                  state.project.sharedWith
+                )
+              } />
           </Segment>
           <Segment attatched='bottom'>
-            <Button color='black' disabled={disabled} loading={loading} fluid content='Save' onClick={submitButtonHandler}/>
+            <Button color='black' disabled={disabled} loading={loading} fluid content='Save' onClick={submitButtonHandler} />
           </Segment>
         </Segment.Group>
       </Modal.Content>
