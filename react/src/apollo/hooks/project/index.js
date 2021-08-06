4c967fb4cfc3097d1fe4cfab9317768d874677b9
Anna Guo <annaguo8010@gmail.com>
2021-08-06 16:44:08 -0400

    add a hook for querying all saved plots of curated projects

--- react/src/apollo/hooks/project/index.js
+++ react/src/apollo/hooks/project/index.js
@@ -1,9 +1,11 @@
 import useCuratedProjectsQuery from './useCuratedProjectsQuery'
 import useProjectRunsQuery from './useProjectRunsQuery'
 import useProjectDetailsQuery from './useProjectDetailsQuery'
+import useCuratedProjectPlotsQuery from './useCuratedProjectPlotsQuery'
 
 export {
   useCuratedProjectsQuery,
   useProjectRunsQuery,
   useProjectDetailsQuery,
+  useCuratedProjectPlotsQuery,
 }
---
+++ react/src/apollo/hooks/project/useCuratedProjectPlotsQuery.js
@@ -0,0 +1,58 @@
+import {useState} from 'react';
+
+import * as RA from 'ramda-adjunct'
+
+import { useQuery } from '@apollo/react-hooks'
+import { gql } from 'apollo-boost'
+
+export default function useCuratedProjectPlotsQuery() {
+  const [curatedProjects, setCuratedProjects] = useState(null)
+
+  const {loading, data, error} = useQuery(gql`
+    query {
+      curatedProjects {
+        projectID
+        name
+        createdOn
+        createdBy {
+          name
+          userID
+        }
+        allDatasets {
+          datasetID
+          name
+        }
+        runs {
+          name
+          runID
+          createdOn
+          createdBy {
+            name
+          }
+          savedPlotQueries {
+            id
+            activeResult
+            selectedQC
+            selectedFeature
+            selectedFeatures
+            selectedGroup
+            selectedAssay
+            selectedDiffExpression
+            selectedQCDataset
+            selectedScaleBy
+            selectedExpRange
+          }
+        }
+      }
+    }
+  `, {
+    fetchPolicy: 'cache-and-network',
+    onCompleted: ({curatedProjects}) => {
+      if (RA.isNotNil(curatedProjects)) {
+        setCuratedProjects(curatedProjects)
+      }
+    }
+  })
+
+  return curatedProjects
+}
