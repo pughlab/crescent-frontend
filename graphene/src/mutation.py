66f70139afc6b2a36fce467ccf67e1c2d37eb88b
suluxan <suluxan.m@gmail.com>
2021-10-24 20:00:21 -0400

    add mutation to graphene schema

--- graphene/src/mutation.py
+++ graphene/src/mutation.py
@@ -3,13 +3,15 @@ from graphene import Schema, ObjectType, String, Field, ID
 #from schema.create_minio_bucket import CreateMinioBucket
 from schema.submit_run import SubmitRun
 from schema.submit_gsva import SubmitGsva
+from schema.submit_inferCNV import SubmitInfercnv
+
 
 class Mutation(ObjectType):
     # Mutation type definition and resolver
     # Note using Field method here from object we expect
     submit_run = SubmitRun.Field()
     submit_gsva = SubmitGsva.Field()
-
+    submit_inferCNV = SubmitInfercnv.Field()
 
 
 
