cc814b0fa874f7519f03f6313885df8de86f985f
annna <annaguo8010@gmail.com>
2022-01-25 18:41:18 -0800

    make plots in multiplot page resizable/draggable

--- react/src/components/main/resultsPage/visualizations/index.js
+++ react/src/components/main/resultsPage/visualizations/index.js
@@ -1,8 +1,11 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import * as R from 'ramda'
 import * as R_ from 'ramda-extension'
-import {Grid, Header, Icon, Image, Segment} from 'semantic-ui-react'
+import {Header, Icon, Image, Segment, Card, Popup, Button} from 'semantic-ui-react'
+import RGL, {WidthProvider} from 'react-grid-layout'
+import {PlotResizeHandle} from './ResponsivePlot'
 import Shake from 'react-reveal/Shake'
+import Fade from 'react-reveal/Fade'
 import {useResultsPage, useCrescentContext} from '../../../../redux/hooks'
 import {useResultsPagePlotQuery} from '../../../../redux/hooks/useResultsPage'
 import {useResultsAvailableQuery} from '../../../../apollo/hooks/results'
@@ -19,6 +22,10 @@ import CrescentPlotCaption from './PlotCaption'
 
 import Tada from 'react-reveal/Tada'
 import Logo from '../../../login/logo.jpg'
+
+// CSS import for react-grid-layout
+import 'react-grid-layout/css/styles.css'
+const GridLayout = WidthProvider(RGL)
 
 export const CrescentPlot = ({
   plotQueryIndex
@@ -39,6 +46,7 @@ const VisualizationsComponent = () => {
   const {runID} = useCrescentContext()
   const {activeResult, activePlot, plotQueries, sidebarCollapsed, runID: compareRunID, runStatus} = useResultsPage()
   const {plots, startResultsPolling, stopResultsPolling} = useResultsAvailableQuery(runID || compareRunID)
+  const [isDragging, setIsDragging] = useState(false)
 
   useEffect(() => {
     startResultsPolling(1000)
@@ -115,33 +123,79 @@ const VisualizationsComponent = () => {
               <CrescentPlot plotQueryIndex={activePlot} />
             </Segment>
           ) : (
-            <Grid columns={R_.ltThanLength(1, plotQueries) ? 2 : 1}>
-              { R.addIndex(R.map)(
-                (plotQuery, idx) => (
-                  <Grid.Column key={idx}>
-                    <Segment
-                      attached="top"
-                      color="violet"
-                      style={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        height: '60vh'
-                      }}
-                    >
-                      <CrescentPlot plotQueryIndex={idx} />
-                    </Segment>
-                    <Segment
-                      compact
-                      attached="bottom"
-                      color="violet"
-                    >
-                      <CrescentPlotCaption plotQueryIndex={idx} />
-                    </Segment>
-                  </Grid.Column>
-                ),
-                plotQueries
-              )}
-            </Grid>
+            <Fade duration={2000}>
+              <Segment basic>
+                <GridLayout
+                  cols={100}
+                  draggableHandle=".plot-draggable-handle"
+                  margin={[25, 10]}
+                  onDrag={() => setIsDragging(true)}
+                  onDragStop={() => setIsDragging(false)}
+                  resizeHandle={<PlotResizeHandle />}
+                  rowHeight={1}
+                >
+                  { R.addIndex(R.map)(
+                    (plotQuery, idx) => (
+                      <div
+                        key={idx}
+                        data-grid={{
+                          x: (idx * 50) % 100,
+                          y: Math.floor(idx / 2) * 46,
+                          w: 50,
+                          h: 46, // Note: the height accounts for the 10px margin between every row; equaivalent to (46 * 1) + ((46 - 1) * 10) = 496px
+                          minW: 33,
+                          minH: 28 // Note: the minimum height accounts for the 10px margin between every row; equivalent to (28 * 1) + ((28 - 1) * 10) - 298px
+                        }}
+                      >
+                        <Card
+                          fluid
+                          color="violet"
+                          style={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            height: '100%',
+                            margin: 0
+                          }}
+                        >
+                          <Popup
+                            disabled={isDragging}
+                            position="top left"
+                            size="small"
+                            trigger={
+                              <Button
+                                className="plot-draggable-handle"
+                                color="violet"
+                                style={{flexShrink: 0}}
+                              >
+                                <Icon
+                                  name="arrows alternate"
+                                  size="large"
+                                />
+                              </Button>
+                            }
+                          >
+                            Drag to move plot
+                          </Popup>
+                          <Card.Content style={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            height: '100%',
+                            marginTop: 0,
+                            overflow: 'hidden auto'
+                          }}>
+                            <CrescentPlot plotQueryIndex={idx} />
+                          </Card.Content>
+                          <Card.Content compact style={{marginBottom: 0}}>
+                            <CrescentPlotCaption plotQueryIndex={idx} />
+                          </Card.Content>
+                        </Card>
+                      </div>
+                    ),
+                    plotQueries
+                  )}
+                </GridLayout>
+              </Segment>
+            </Fade>
           )}
         </>
       )}
